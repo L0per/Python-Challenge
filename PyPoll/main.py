6a2dcb39d0ae5caff7aefba2a6d407306f9c5768
Loper <jloper@ucsc.edu>
2019-10-07 17:14:50 -0700

    prints winning candidate

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,8 +9,30 @@ with open("election_data.csv") as file:
 
 # Casting
 total_votes = 0
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
 
 # Calculations
+for row in election_data:
+    total_votes += 1 # Total number of votes
+    # Total votes for each candidate
+    if row[2] == "Khan":
+        khan_votes += 1
+    elif row[2] == "Correy":
+        correy_votes += 1
+    elif row[2] == "Li":
+        li_votes += 1
+    elif row[2] == "O'Tooley":
+        otooley_votes += 1
+
+# Create dictionary with candidate votes, used for finding winner
+max_dict = {"Khan" : khan_votes, "Correy" : correy_votes, "Li" : li_votes, "O'Tooley" : otooley_votes}
+
+# Find max value in dictionary and prints associated key (candidate)
+print(max(max_dict, key=max_dict.get))
+
 
 
 #  Election Results
