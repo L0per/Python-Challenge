7839e8cceeb561ec645b320130462bcf68fc38c1
Loper <jloper@ucsc.edu>
2019-10-08 21:13:36 -0700

    prints to csv

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,40 +9,46 @@ with open("election_data.csv") as file:
 
 # Casting
 total_votes = 0
-khan_votes = 0
-correy_votes = 0
-li_votes = 0
-otooley_votes = 0
 
-# Calculations
+# Create dictionary for tallying votes
+vote_dict = {}
 for row in election_data:
-    total_votes += 1 # Total number of votes
-    # Total votes for each candidate
-    if row[2] == "Khan":
-        khan_votes += 1
-    elif row[2] == "Correy":
-        correy_votes += 1
-    elif row[2] == "Li":
-        li_votes += 1
-    elif row[2] == "O'Tooley":
-        otooley_votes += 1
+    total_votes += 1
+    if row[2] not in vote_dict.keys():
+        vote_dict[row[2]] = 1
+    else:
+        vote_dict[row[2]] += 1
 
-# Create dictionary with candidate votes, used for finding winner
-max_dict = {"Khan" : khan_votes, "Correy" : correy_votes, "Li" : li_votes, "O'Tooley" : otooley_votes}
+# Find max value in dictionary
+winner = max(vote_dict, key=vote_dict.get)
 
-# Find max value in dictionary and prints associated key (candidate)
-print(max(max_dict, key=max_dict.get))
+# Print results to terminal
+print(f'''
+Election Results
+-------------------------
+Total Votes: {total_votes}
+-------------------------
+{list(vote_dict.keys())[0]}: {round((vote_dict[list(vote_dict.keys())[0]]/total_votes)*100,2)}% ({vote_dict[list(vote_dict.keys())[0]]})
+{list(vote_dict.keys())[1]}: {round((vote_dict[list(vote_dict.keys())[1]]/total_votes)*100,2)}% ({vote_dict[list(vote_dict.keys())[1]]})
+{list(vote_dict.keys())[2]}: {round((vote_dict[list(vote_dict.keys())[2]]/total_votes)*100,2)}% ({vote_dict[list(vote_dict.keys())[2]]})
+{list(vote_dict.keys())[3]}: {round((vote_dict[list(vote_dict.keys())[3]]/total_votes)*100,2)}% ({vote_dict[list(vote_dict.keys())[3]]})
+-------------------------
+Winner: {winner}
+-------------------------
+''')
 
-
-
-#  Election Results
-#  -------------------------
-#  Total Votes: 3521001
-#  -------------------------
-#  Khan: 63.000% (2218231)
-#  Correy: 20.000% (704200)
-#  Li: 14.000% (492940)
-#  O'Tooley: 3.000% (105630)
-#  -------------------------
-#  Winner: Khan
-#  -------------------------
+# Print to CSV
+with open("election_data_summary.csv", 'w', newline='') as newfile:
+    newfilewriter = csv.writer(newfile)
+    newfilewriter.writerows([
+        ["Election Results"],
+        ["-------------------------"],
+        [f'Total Votes: {total_votes}'],
+        ["-------------------------"],
+        [f'{list(vote_dict.keys())[0]}: {round((vote_dict[list(vote_dict.keys())[0]]/total_votes)*100,2)}% ({vote_dict[list(vote_dict.keys())[0]]})'],
+        [f'{list(vote_dict.keys())[1]}: {round((vote_dict[list(vote_dict.keys())[1]]/total_votes)*100,2)}% ({vote_dict[list(vote_dict.keys())[1]]})'],
+        [f'{list(vote_dict.keys())[2]}: {round((vote_dict[list(vote_dict.keys())[2]]/total_votes)*100,2)}% ({vote_dict[list(vote_dict.keys())[2]]})'],
+        [f'{list(vote_dict.keys())[3]}: {round((vote_dict[list(vote_dict.keys())[3]]/total_votes)*100,2)}% ({vote_dict[list(vote_dict.keys())[3]]})'],
+        ["-------------------------"],
+        [f'Winner: {winner}'],
+        ["-------------------------"]])
